7125ef78d41f426e2057afe865aae5e5530a6d54
Usaid Asif <usaidasif12@gmail.com>
2024-07-20 20:38:27 +0500

    add sha for google sign in for android | added text

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -14,60 +14,88 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Center(
-            child: Image.asset('images/gdrive.png'),
-          ),
-          const Text(
-            'Google Drive Clone',
-            style: TextStyle(
-              fontSize: 35,
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Image.asset('images/gdrive.png'),
+                ),
+                const Text(
+                  'Google Drive Clone',
+                  style: TextStyle(
+                    fontSize: 35,
+                  ),
+                ),
+                const SizedBox(
+                  height: 70,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 50, right: 50),
+                  child: MaterialButton(
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    elevation: 10,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 50.0,
+                          width: 50.0,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    'https://cdn-icons-png.flaticon.com/512/2991/2991148.png'),
+                                fit: BoxFit.cover),
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 40,
+                        ),
+                        Text(
+                          "Sign In with Google",
+                          style: TextStyle(
+                            color: Colors.indigo[900],
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    onPressed: () async {
+                      AuthService().signInWithGoogle();
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const HomeScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
-          const SizedBox(
-            height: 70,
-          ),
           Padding(
-            padding: const EdgeInsets.only(left: 50, right: 50),
-            child: MaterialButton(
-              color: const Color.fromARGB(255, 255, 255, 255),
-              elevation: 10,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    height: 40.0,
-                    width: 40.0,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: NetworkImage(
-                              'https://cdn-icons-png.flaticon.com/512/2991/2991148.png'),
-                          fit: BoxFit.cover),
-                      shape: BoxShape.circle,
-                    ),
+            padding: const EdgeInsets.only(bottom: 50),
+            child: Column(
+              children: [
+                Text(
+                  'Made By',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
                   ),
-                  const SizedBox(
-                    width: 40,
+                ),
+                Text(
+                  'Usaid Asif',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 35,
                   ),
-                  Text(
-                    "Sign In with Google",
-                    style: TextStyle(
-                      color: Colors.indigo[900],
-                      fontSize: 20,
-                    ),
-                  ),
-                ],
-              ),
-              onPressed: () async {
-                AuthService().signInWithGoogle();
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => HomeScreen(),
-                  ),
-                );
-              },
+                ),
+              ],
             ),
           ),
         ],
